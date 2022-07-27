1e412615011274fbacb5eb5bb8fffbdf2f90842f
Canel420 <4521@holbertonschool.com>
2022-07-27 08:58:38 -0500

    removed unnecessary brackets

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,9 +12,8 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL){
+	if (tree == NULL || func == NULL)
 		return;
-	}
 
 	func(tree->n);
 
